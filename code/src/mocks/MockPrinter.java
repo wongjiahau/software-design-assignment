1dfeac7f43176e6204eb830e60a2df7d8d3daf19
Wong Jia Hau <hou32hou@gmail.com>
2018-03-23 11:31:34 +0800

    Added mocks

---
+++ code/src/mocks/MockPrinter.java
@@ -0,0 +1,24 @@
+package mocks;
+
+
+import java.util.ArrayList;
+
+import boundary.IPrinter;
+
+public class MockPrinter implements IPrinter {
+    private ArrayList<String> printedLines;
+    public MockPrinter() {
+        this.printedLines = new ArrayList<String>();
+    }
+
+	@Override
+	public void print(String message) {
+        this.printedLines.add(message);
+    }
+
+	@Override
+	public void printLine(String message) {
+        this.printedLines.add(message);
+	}
+
+}
